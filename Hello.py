368a55ab171541f852f75f7319deabebbfefdc97
laxmiprasanna27 <laxmiprasanna12244@gmail.com>
2025-07-09 10:46:49 +0530

    commit 2

--- Hello.py
+++ Hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-	return ("Hello world")
+	return ("Hello world have a nice daya!!")
 if __name__=='__main__':
     app.run(debug=True)
